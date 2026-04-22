9de171c288939f34cb751bbce218113c7a970e83
kominjung04 <144197755+kominjung04@users.noreply.github.com>
2026-04-22 11:05:48 +0900

    [level 0] Title: 코드 처리하기, Time: 373.28 ms, Memory: 373 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        String ret="";
+        int mode = 0;
+        for(int i = 0 ; i < code.length();i++){
+            if(code.charAt(i) == '1') {mode = 1 - mode;continue;}
+            if(mode == 0 && i % 2 == 0 ) ret+=code.charAt(i);
+            else if(mode == 1 && i%2 == 1) ret+=code.charAt(i);
+        }
+        if(ret.equals("")) ret = "EMPTY";
+        
+        answer = ret;
+        return answer;
+    }
+}
